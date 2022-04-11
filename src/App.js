d795ee4b2bba64bcaa2cddb834bd9921ae606054
Akashparihar470 <96344598+Akashparihar470@users.noreply.github.com>
2022-04-11 20:56:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
+  const opr = ["Android","Blackberry","iphone", "Windows Phone"];
+  const man = ["samsung","HTC","Micromax","Apple"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Mobile Operating System</h1>
+     {opr.map((el) =>
+       <li>{el}</li>
+     )}
+     <h1>Mobile Manufacturers</h1>
+     {man.map((el) =>(
+       <li className={`${el === "HTC" || el === "samsung"? "squaree": "circlee"}`}>
+         {el}</li>
+     ))}
     </div>
   );
 }
